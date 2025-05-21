00afe19bea6224f637b21256dceba6e6fb59755d
Chomty <chomty21@gmail.com>
2025-05-21 10:25:16 +0900

    first commit

---
+++ src/Table.java
@@ -0,0 +1,34 @@
+enum Stone {
+    BLACK,
+    WHITE,
+    EMPTY
+}
+class Omok_Table{
+    int[][] Omok_Vecter = new int[15][15];
+
+}
+
+class Act{
+    /**
+     * @param MyStone : Stone(enum)
+     * 주어진 돌을 반전해서 반환함.
+     * @return 반대색의 돌 (BLACK, WHITE)
+     * */
+    public Stone Invert_Stone(Stone MyStone){
+        if(MyStone == Stone.BLACK){
+            return Stone.WHITE;
+        } else if (MyStone == Stone.WHITE) {
+            return Stone.BLACK;
+        }
+        return Stone.EMPTY; // 변환 못하면 빈 값 출력
+    }
+
+
+
+}
+
+public class Table {
+    Stone MyStone = Stone.EMPTY;
+
+
+}
