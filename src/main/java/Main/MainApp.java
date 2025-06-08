530e1dbe36268c00355e17b97fd461f7a3d38890
Chomty <chomty21@gmail.com>
2025-06-09 04:06:13 +0900

    Im winner

---
+++ src/main/java/Main/MainApp.java
@@ -0,0 +1,32 @@
+package Main;
+
+import Layout.RootLayout;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+import java.util.Objects;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Scene scene = new Scene(RootLayout.createRoot(1280, 720));
+        // css 스타일 아이콘 적용
+        scene.getStylesheets().add(
+                Objects.requireNonNull(
+                        getClass().getResource("/Style.css")
+                ).toExternalForm());
+
+        stage.setTitle("Omok V1");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+    }
+
+}
