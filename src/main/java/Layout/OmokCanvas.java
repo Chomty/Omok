58acaa960065ce2226f78ac4c6f48b8495bd99c4
Chomty <chomty21@gmail.com>
2025-06-09 20:52:05 +0900

    help

--- src/main/java/Layout/OmokCanvas.java
+++ src/main/java/Layout/OmokCanvas.java
@@ -5,7 +5,12 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
 public class OmokCanvas {
-
+    /**
+     * @param size : (int) 격자 갯수
+     * @param cellSize : (int) 버튼 사이즈(기준)
+     *
+     * @return 격자 Canvas를 리턴함
+     * */
     public static Canvas createBoard(int size, double cellSize) {
         double boardSize = cellSize * (size - 1);  // 격자 수 기준으로 크기 설정
         Canvas canvas = new Canvas(boardSize + 43, boardSize + 43); // 여유 여백
